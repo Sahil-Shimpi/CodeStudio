da460fc9384a5fb641729b337e90e3b1e9411387
Sahil Shimpi <65404265+Sahil-Shimpi@users.noreply.github.com>
2022-02-11 13:16:39 +0530

    Create 002. Print Name and age.cpp

---
+++ 002. Print Name and age.cpp
@@ -0,0 +1,14 @@
+// Link to the problem: https://www.codingninjas.com/codestudio/guided-paths/basics-of-c/content/118781/offering/1380852
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    string name;
+    int age;
+    cin >> name >> age;
+    cout << "The name of the person is " << name << " and the age is " << age << "." << "\n";
+
+    return 0;
+}
