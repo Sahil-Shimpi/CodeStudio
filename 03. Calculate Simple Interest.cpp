14527bd27f81db8e88befe4bbac172375ad26337
Sahil Shimpi <65404265+Sahil-Shimpi@users.noreply.github.com>
2022-02-11 13:27:05 +0530

    Create 03. Calculate Simple Interest.cpp

---
+++ 03. Calculate Simple Interest.cpp
@@ -0,0 +1,14 @@
+// Link to the Problem: https://www.codingninjas.com/codestudio/guided-paths/basics-of-c/content/118781/offering/1380854
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int p, n;
+    float r;
+    cin >> p >> r >> n;
+    cout << (int)(p*n*r/100);
+
+    return 0;
+}
