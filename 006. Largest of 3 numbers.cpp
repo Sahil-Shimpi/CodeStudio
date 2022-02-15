17303dbe7e47317a93ae9188398535a2e979e1d1
Sahil Shimpi <65404265+Sahil-Shimpi@users.noreply.github.com>
2022-02-15 11:06:35 +0530

    Create 006. Largest of 3 numbers.cpp

---
+++ 006. Largest of 3 numbers.cpp
@@ -0,0 +1,25 @@
+// Problem: https://www.codingninjas.com/codestudio/guided-paths/basics-of-c/content/118784/offering/1381042?leftPanelTab=0
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a,b,c;
+    cin >> a >> b >> c;
+    
+    if (a > b && a > c) 
+    {
+        cout << a << endl;
+    }
+    else if (b > a && b > c)
+    {
+        cout << b << endl;
+    }
+    else
+    {
+        cout << c << endl;
+    }
+
+    return 0;
+}
