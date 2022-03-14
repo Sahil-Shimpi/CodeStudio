b14a503c99ed707bb0a66c77c298712d71b4621f
Sahil Shimpi <65404265+Sahil-Shimpi@users.noreply.github.com>
2022-03-15 01:28:21 +0530

    Update 068. First Unique Character in a String.cpp

--- 068. First Unique Character in a String.cpp
+++ 068. First Unique Character in a String.cpp
@@ -1,38 +1,22 @@
 // Problem: https://www.codingninjas.com/codestudio/guided-paths/data-structures-algorithms/content/118626/offering/1382157
 
-/*
-	Time complexity: O(N)
-	Space complexity: O(1)
+#include <unordered_map>
 
-	Where N is the length of the string 
-*/
-
-// Calculate count of characters in the string str
-vector<int> getcharCount(string str) 
-{
-    vector<int> charCount(257,0);
-
-    for (int i = 0; i < str.size(); i++) 
-    {
-        charCount[str[i]-'a']++;
-    }
-
-    return charCount;
-}
-
-// Returns first non-repeating character in a string, else returns '#'
 char findNonRepeating(string str) 
 {
-    vector<int> charCount;
-    charCount = getcharCount(str);
-
-    for (int i = 0; i < str.size(); i++) 
+    int n = str.length();
+    unordered_map <char, int> freq;
+    for(int i=0; i<n; i++)
     {
-        if (charCount[str[i]-'a'] == 1) 
+        char temp = str[i];
+        freq[temp]++;
+    }
+    for(int i=0; i<n; i++)
+    {
+        if(freq[str[i]] == 1)
         {
             return str[i];
         }
     }
-
     return '#';
 }
