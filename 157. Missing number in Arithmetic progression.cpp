8bb319aecefc6c5aec98268aa00076eb589d160d
Sahil Shimpi <65404265+Sahil-Shimpi@users.noreply.github.com>
2022-04-08 23:16:23 +0530

    Update 157. Missing number in Arithmetic progression.cpp

--- 157. Missing number in Arithmetic progression.cpp
+++ 157. Missing number in Arithmetic progression.cpp
@@ -1,42 +1,53 @@
 // Problem: https://www.codingninjas.com/codestudio/guided-paths/data-structures-algorithms/content/118621/offering/1377969?leftPanelTab=2
 
+/*
+	Time Complexity: O(log(N))
+	Space complexity: O(1)
+
+	Where 'N' is the missing of elements in the given sequence.
+*/
+
 int missingNumber(vector<int> &arr, int n) 
-{
-	int a = arr[0];
-	int d;
-	
-	if(arr[1] - arr[0] == arr[2] - arr[1])
-	{
-		d = arr[1] - arr[0];
-	}
-	else if(arr[2] - arr[1] == 2 * (arr[1] - arr[0]))
-	{
-		d = arr[1] - arr[0];
-	}
-	else if(arr[1] - arr[0] == 2 * (arr[2] - arr[1]))
-	{
-		d = arr[2] - arr[1];
-	}
-	
-	int low = 0;
-	int high = n-1;
-	int mid;
-	
-	while(low < high)
-	{
-		mid = low + (high - low)/2;
-		
-		if(arr[mid] - arr[mid - 1] == 2 * d)
-		{
-			return arr[mid] - d;
-		}
-		else if(arr[mid] == a + (mid * d))
-		{
-			low = mid + 1;
-		}
-		else
-		{
-			high = mid - 1;
-		}
-	}
+{   
+    /* 
+      The first term and (n+1)th term of the AP will be arr[0] and arr[n-1].
+      So using the formula the common difference d will be given by d = (arr[n-1] - arr[0]) / n
+    */
+    int a = arr[0];
+    int d = (arr[n - 1] - arr[0]) / n;
+    int l = 0, r = n - 1;
+    int missing = -1;
+    
+    while (l <= r) 
+    {
+        long int mid = (l + r) / 2;
+        
+        // Check if the difference between the adjacent elements of the mid is same as the common difference or not
+        if (mid + 1 < n && arr[mid + 1] - arr[mid] != d) 
+        {
+            missing = arr[mid] + d;
+            break;
+        }
+        
+        if (mid - 1 >= 0 && arr[mid] - arr[mid - 1] != d) 
+        {
+            missing = arr[mid - 1] + d;
+            break;
+        }
+
+        // Check If the difference is not same as the common difference of the AP
+        if (arr[mid] == a + mid * d) 
+        {
+            l = mid + 1;
+        } 
+        else 
+        {
+            r = mid - 1;
+        }
+    }
+
+    // Return the missing number
+    return missing;
 }
+
+
